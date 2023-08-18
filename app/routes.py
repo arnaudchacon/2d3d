c82c48a65716f4b3865df1ddd24e8d175f42aa52
arnaudchacon <arnaudchacon@gmail.com>
2023-08-18 14:13:18 +0900

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,35 @@
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from app import app
+import os
+
+UPLOAD_FOLDER = 'app/static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config:
+    SECRET_KEY = 'my_precious_secret_key'
